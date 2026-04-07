f835a47924c7dcbe7fe1817e7830018dcc0fd4b0
Saikamal45 <saikamal2727@gmail.com>
2026-04-07 14:53:15 +0530

    07/04/2026

--- src/main/java/com/demo/mappings/controller/UserController.java
+++ src/main/java/com/demo/mappings/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.demo.mappings.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.demo.mappings.entity.Teacher;
 import com.demo.mappings.entity.User;
 import com.demo.mappings.service.UserService;
--- src/main/java/com/demo/mappings/serviceImple/UserServiceImplementation.java
+++ src/main/java/com/demo/mappings/serviceImple/UserServiceImplementation.java
@@ -51,5 +51,6 @@ public class UserServiceImplementation implements UserService{
 	    return teacherRepo.save(teacher);
 	}
 
+	//checking cicd
 
 }
